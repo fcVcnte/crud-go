d1da1e4920fabd8fd3cc603aab3a90d353288444
fcVcnte <vcnte.felipe@outlook.com>
2023-08-01 21:00:08 -0300

    error struct pattern created.

---
+++ src/configuration/err/err.go
@@ -0,0 +1,61 @@
+package err
+
+import "net/http"
+
+type Err struct {
+	Message string `json: "message"`
+	HttpDescr string `json: "httpDescr"`
+	HttpCode int `json: "httpCode"`
+	Causes []Causes `json: "causes,omitempty"`
+}
+
+type Causes struct {
+	Field string `json: "field"`
+	Message string `json: "message"`
+}
+
+func (err *Err) Error() string {
+	return err.Message
+}
+
+func NewError(message, httpDescr string, httpCode int, causes []Causes) *Err {
+	return &Err {
+		Message: message,
+		HttpDescr: httpDescr,
+		HttpCode: httpCode,
+		Causes: causes,
+	}
+}
+
+func NewBadRequestError(message string) *Err {
+	return &Err {
+		Message: message,
+		HttpDescr: "Bad Request",
+		HttpCode: http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *Err {
+	return &Err {
+		Message: message,
+		HttpDescr: "Bad Request",
+		HttpCode: http.StatusBadRequest,
+		Causes: causes,
+	}
+}
+
+func NewInternalServerError(message string) *Err {
+	return &Err {
+		Message: message,
+		HttpDescr: "Internal Server Error",
+		HttpCode: http.StatusInternalServerError,
+	}
+}
+
+func NewForbiddenError(message string) *Err {
+	return &Err {
+		Message: message,
+		HttpDescr: "Forbidden",
+		HttpCode: http.StatusForbidden,
+	}
+}
